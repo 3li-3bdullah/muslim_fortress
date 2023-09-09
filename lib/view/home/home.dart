6ad7234e87ddc87fd77f615e7e72909959c214ec
3li-3bdullah <PROGRMMER7@gmail.com>
2023-09-09 15:35:44 +0200

    Removed the old code

--- lib/view/home/home.dart
+++ lib/view/home/home.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class Home extends StatelessWidget {
+import '../../controller/home_controller.dart';
+
+class Home extends GetWidget<HomeController> {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.pink.shade200,
+      body: Center(
+        child: Container(
+          height: 200,
+          width: 250,
+          alignment: Alignment.center,
+          color: Colors.white,
+          child: const Text(
+            "الصفحة الرئيسية",
+            style: TextStyle(
+                color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
   }
 }
