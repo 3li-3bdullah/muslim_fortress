89e64a732ff4ee2da03baafe193c0b0b6eee9378
3li-3bdullah <PROGRMMER7@gmail.com>
2023-09-14 16:08:31 +0200

    Added some widgets

--- lib/view/home/home.dart
+++ lib/view/home/home.dart
@@ -9,20 +9,32 @@ class Home extends GetWidget<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.pink.shade200,
-      body: Center(
-        child: Container(
-          height: 200,
-          width: 250,
-          alignment: Alignment.center,
-          color: Colors.white,
-          child: const Text(
-            "الصفحة الرئيسية",
+        key: controller.scaffoldKey,
+        backgroundColor: Colors.yellow.shade100,
+        appBar: AppBar(
+          backgroundColor: Colors.brown,
+          centerTitle: true,
+          title: const Text(
+            "حصن المسلم",
             style: TextStyle(
-                color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold),
+          ),
+          leading: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.search_rounded,
+                color: Colors.white,
+                size: 25,
+              )),
+        ),
+        endDrawer: Drawer(
+          child: Container(
+            color: Colors.brown,
+            width: 200,
           ),
         ),
-      ),
-    );
+        body: Container());
   }
 }
