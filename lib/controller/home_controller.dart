ace20b71c0fc98430bb72a6321a4b8f36aabcb19
3li-3bdullah <PROGRMMER7@gmail.com>
2023-09-14 15:40:33 +0200

    Added some logic

--- lib/controller/home_controller.dart
+++ lib/controller/home_controller.dart
@@ -1,15 +1,18 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
-class HomeController extends GetxController {
+class HomeController extends GetxController { 
   List<dynamic> adhkarList = [];
+  GlobalKey scaffoldKey = GlobalKey();
 
   @override
   void onInit() {
     super.onInit();
     getJsonData();
+    // getData();
   }
 
   getJsonData() async {
@@ -19,4 +22,17 @@ class HomeController extends GetxController {
     adhkarList = jsonDecode(azkarData);
     update();
   }
+
+  // getData() async {
+  //   // const url = "http://localhost:3000/api/employees/list";
+  //   try {
+
+  //   http.Response response = await http.get(Uri.http('localhost:3000','/api/employees/list'));
+
+  //   var body = json.decode(response.body);
+  //   print(body);
+  //   } catch (e) {
+  //     print("The Error is : $e");
+  //   }
+  // }
 }
