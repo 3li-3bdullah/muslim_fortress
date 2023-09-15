5ce7ebece7cc243acaff0006a035534ccd5d4bff
3li-3bdullah <PROGRMMER7@gmail.com>
2023-09-15 18:39:53 +0200

    Added ListView.builder and some edit

--- lib/view/home/reading_page.dart
+++ lib/view/home/reading_page.dart
@@ -1,22 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:get/get.dart';
 import 'package:muslim_fortress/shared/components/constants.dart';
 import 'package:muslim_fortress/shared/components/custom_text.dart';
-import 'package:muslim_fortress/view/home/components/custom_appbar.dart';
 
 class ReadingPage extends StatelessWidget {
-  const ReadingPage({super.key, required this.text});
-
-  final String text;
+  const ReadingPage({super.key, required this.texts});
+  final List texts;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       body: Container(
+        height: Get.height,
+        width: Get.width,
         color: kbackgroundColor,
-        child: CustomText(text: text, fontWeight: FontWeight.bold,color: Colors.black,),
+        alignment: Alignment.center,
+        child: ListView.builder(
+            itemCount: texts.length,
+            itemBuilder: (context, index) => CustomText(
+                  text: texts[index]['text'],
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                )),
       ),
     );
   }
