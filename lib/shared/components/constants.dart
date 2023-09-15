ea1ae040c9f65107477be1c0f0d21e3227a251f2
3li-3bdullah <PROGRMMER7@gmail.com>
2023-09-15 18:26:07 +0200

    Added some colors

--- lib/shared/components/constants.dart
+++ lib/shared/components/constants.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+
+const Color kbackgroundColor = Color.fromRGBO(255, 249, 196, 1);
+const Color kmainColor = Colors.brown;
+
 void dpr(dynamic data){
    debugPrint(data);
 }
