82446631322313fbc69b0c25202cca2de8cacd25
3li-3bdullah <PROGRMMER7@gmail.com>
2023-09-17 18:41:51 +0200

    Added new property

--- lib/shared/components/custom_text.dart
+++ lib/shared/components/custom_text.dart
@@ -6,16 +6,19 @@ class CustomText extends StatelessWidget {
       required this.text,
       this.color = Colors.white,
       required this.fontWeight,
+      this.maxLines = 1,
       this.fontSize = 18});
   final String text;
   final Color color;
   final FontWeight fontWeight;
   final double fontSize;
+  final int maxLines;
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
+      maxLines: maxLines,overflow: TextOverflow.ellipsis,
       style:
           TextStyle(color: color, fontSize: fontSize, fontWeight: fontWeight),
     );
