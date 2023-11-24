eaff13f0e480ea09e1f9c3a89341eba441c66d95
3li-3bdullah <PROGRMMER7@gmail.com>
2023-11-24 12:18:35 +0200

    Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:muslim_fortress/shared/components/size_config.dart';
 import 'package:muslim_fortress/view/home/home.dart';
+import 'package:muslim_fortress/view/home/test_canvas.dart';
+import 'package:muslim_fortress/view/test/load_page.dart';
+import 'package:muslim_fortress/view/test/test_multithreads.dart';
 import '../shared/my_bindings/my_bindings.dart';
 
 void main() {
@@ -12,6 +16,7 @@ class MuslimFortress extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // SizeConfig().init(context);
     return GetMaterialApp(
       title: "Muslim's Fortress",
       debugShowCheckedModeBanner: false,
@@ -19,7 +24,7 @@ class MuslimFortress extends StatelessWidget {
         useMaterial3: true,
       ),
       initialBinding: MyBindings(),
-      home: const Home(),
+      home: const TestMulti(),
     );
   }
 }
