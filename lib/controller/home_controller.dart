56a14f762d590c8a8a9cfb7f202dba90a49bd559
3li-3bdullah <PROGRMMER7@gmail.com>
2023-11-24 12:18:14 +0200

    Added

--- lib/controller/home_controller.dart
+++ lib/controller/home_controller.dart
@@ -1,11 +1,14 @@
 import 'dart:convert';
+import 'dart:isolate';
+import 'dart:ui';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
-  List<dynamic> adhkarList = [];
+  List<Map<String, dynamic>> adhkarList = [];
   GlobalKey scaffoldKey = GlobalKey();
 
   @override
@@ -16,9 +19,49 @@ class HomeController extends GetxController {
   }
 
   getJsonData() async {
-    adhkarList = await rootBundle
-        .loadString("assets/json/adhkar.json")
-        .then((value) => jsonDecode(value));
+    var data = await rootBundle.loadString("assets/json/hisn_almuslim.json");
+
+    var jsonDecoded = json.decode(data);
+    // log(jsonDecoded);
+    int count = 0;
+    jsonDecoded.forEach((key, value) {
+      adhkarList.add({"$count": key, "body": value});
+      // print(adhkarList);
+      count++;
+    });
+    update();
+  }
+
+//* To Do Flutter Package
+  List<int> list = [];
+  int count = 0;
+  loadTenData(int index) async {
+    if (index == list.length - 1) {
+      await Future.delayed(const Duration(seconds: 1));
+      index += 1;
+      for (var i = 1; i <= 10; i++) {
+        list.add(index + i);
+      }
+      update();
+    }
+  }
+
+//* Test Multithreads
+  bool isLoading = false;
+  
+  getData() async {
+    isLoading = true;
+    update();
+//* Here to Create New Thread
+   await Isolate.spawn((message) async {
+    print("At Isolate ;)");
+    await Future.delayed(const Duration(seconds: 5), () => print("5 seconds is done"));
+   },"message" );
+   
+    await Future.delayed(
+        const Duration(seconds: 2), () => print('2 seconds is done'));
+    isLoading = false;
+
     update();
   }
 
